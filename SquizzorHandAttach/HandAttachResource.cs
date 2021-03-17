304632aa3b2af6183ed9dc673257779fa7df3bc3
Mario Kramer <mario-kramer@live.de>
2021-03-17 13:27:08 +0100

    + initial commit

---
+++ SquizzorHandAttach/HandAttachResource.cs
@@ -0,0 +1,18 @@
+﻿using AltV.Net.Async;
+using System;
+
+namespace SquizzorHandAttach
+{
+    public class HandAttachResource : AsyncResource
+    {
+        public override void OnStart()
+        {
+            //
+        }
+
+        public override void OnStop()
+        {
+            //
+        }
+    }
+}
---
+++ SquizzorHandAttach/client/client.js
@@ -0,0 +1,47 @@
+﻿import natives from 'natives';
+import alt from 'alt';
+
+function getScriptId(playerId) {
+    var scriptId = null;
+
+    if (playerId == alt.Player.local.id) {
+        scriptId = alt.Player.local.scriptID;
+    } else {
+        var player = alt.Player.getByID(playerId);
+        if (player) {
+            scriptId = player.scriptID;
+        }
+    }
+
+    return scriptId;
+}
+
+
+alt.on('test', (type) => {
+    console.log('test');
+});
+
+alt.emit('test');
+
+alt.onServer('tslipsync:onStartTalking', (playerId) => {
+    var scriptId = getScriptId(playerId);
+    if (scriptId) {
+        natives.playFacialAnim(scriptId, 'mic_chatter', 'mp_facial');
+    }
+});
+
+alt.onServer('tslipsync:onStopTalking', (playerId) => {
+    var scriptId = getScriptId(playerId);
+    if (scriptId) {
+        var isMale = natives.isPedMale(scriptId);
+        var animationName = '';
+
+        if (isMale) {
+            animationName = 'facials@gen_male@variations@normal';
+        } else {
+            animationName = 'facials@gen_female@variations@normal';
+        }
+
+        natives.playFacialAnim(scriptId, 'mood_normal_1', animationName);
+    }
+});
